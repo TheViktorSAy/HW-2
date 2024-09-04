4129d725b82f5d7b7a6057de59f5167e0109db1f
vkostenko <git@github.com:TheViktorSAy/Hellow-World.git>
2024-09-04 18:32:34 +0300

    task 1/2/3

--- CAT&DOG/Main.java
+++ CAT&DOG/Main.java
@@ -1,22 +1,38 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        var dog = 10;
+// задание 1
+        var dog = 10.0;
         System.out.println(dog);
+
         var cat = 3.6;
         System.out.println(cat);
+
         var paper = 763789;
         System.out.println(paper);
 
-        dog = dog + 4;
-        System.out.println(dog);
+// задание 2
+        var resultDog = dog + 4;
+        System.out.println(resultDog);
 
-        cat = cat + 4;
-        System.out.println(cat);
+        var resultCat = cat + 4;
+        System.out.println(resultCat);
 
-        paper = paper + 4;
-        System.out.println(paper);
+        var resultPaper = paper + 4;
+        System.out.println(resultPaper);
+
+// задание 3
+                resultDog = resultDog - 3.5;
+        System.out.println(resultDog);
+
+        resultCat = resultCat - 1.6;
+        System.out.println(resultCat);
+
+        resultPaper = resultPaper - 7639;
+        System.out.println(resultPaper);
+
+
+
 
 
 
