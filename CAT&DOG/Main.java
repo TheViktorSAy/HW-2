b302c15712f03b5b15a8f742500e667fca5fa30c
vkostenko <git@github.com:TheViktorSAy/Hellow-World.git>
2024-09-04 15:46:58 +0300

    Initial cpmmit

---
+++ CAT&DOG/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        var box = 12;
+        System.out.println(box);
+
+        var result = box / 2 ;
+        System.out.println(result);
+
+    }
+
+
+
+
+}
