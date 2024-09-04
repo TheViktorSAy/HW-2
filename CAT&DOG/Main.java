3cefd2f28c8579b2f4526ad7efb27de854b6d122
vkostenko <git@github.com:TheViktorSAy/Hellow-World.git>
2024-09-04 16:53:01 +0300

    Change box 11

--- CAT&DOG/Main.java
+++ CAT&DOG/Main.java
@@ -6,9 +6,18 @@ public class Main {
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
-
         var paper = 763789;
         System.out.println(paper);
+
+        dog = dog + 4;
+        System.out.println(dog);
+
+        cat = cat + 4;
+        System.out.println(cat);
+
+        paper = paper + 4;
+        System.out.println(paper);
+
 
 
     }
