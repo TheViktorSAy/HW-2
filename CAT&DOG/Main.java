2955d8dcbc63507da83cb52465cd2467fc41c365
vkostenko <git@github.com:TheViktorSAy/Hellow-World.git>
2024-09-04 15:53:25 +0300

    Change box 11

--- CAT&DOG/Main.java
+++ CAT&DOG/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        var box = 12;
+        var box = 11;
         System.out.println(box);
 
         var result = box / 2 ;
